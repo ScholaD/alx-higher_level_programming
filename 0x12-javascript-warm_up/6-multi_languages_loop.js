586345235bc90ca72d4304db66fef8bdb046d11f
ScholaD <ifeomaegbeonu@gmail.com>
2023-11-14 16:31:10 +0000

    Why am i feelibg likr C in Javascript

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// using an array of string and a loop
+
+const langs = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+
+for (let i = 0; i < 3; i++) {
+  console.log(langs[i]);
+}
